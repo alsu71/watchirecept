f139509ab73352da7d92baffd785a58b5c505f50
Alsu <66012600+alsu71@users.noreply.github.com>
2022-03-04 13:47:02 +0300

    Add files via upload

---
+++ rec/rec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rec
+//
+//  Created by Student on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
